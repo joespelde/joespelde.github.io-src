7605c183c7fdc517a280177c6b541f977be3801f
joespelde <josephspelde@gmail.com>
2019-11-02 12:40:45 -0500

    new theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+from datetime import datetime
+from pathlib import Path
+
+CURRENT_DIR_PATH = Path(__file__).resolve().parent
 
 AUTHOR = 'Joe Spelde'
 SITENAME = 'Joe Spelde'
-SITEURL = ''
-
+SITEURL = 'joespelde.github.io'
+THEME = '{}/voce'.format(CURRENT_DIR_PATH)
 PATH = 'content'
 
 TIMEZONE = 'America/Chicago'
-
 DEFAULT_LANG = 'en'
+DEFAULT_PAGINATION = 6
+SUMMARY_MAX_LENGTH = 30
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -19,15 +24,32 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+# Page Settings
+PAGE_SAVE_AS = '{slug}.html'
+TAGS_URL = 'tags.html'
+ARCHIVES_URL = 'archive.html'
+
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Home', '/index.html'),
+         ('About', '/about.html'))
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Email', 'mailto:josephspelde@gmail.com'),
+          ('GitHub', 'http://github.com/joespelde'),
+	  	  ('Linkedin', 'https://www.linkedin.com/in/joseph-spelde-2a3a12133/'))
+
+# Plugins
+PLUGINS = ['assets']
+PLUGIN_PATHS = ['{}/plugins'.format(THEME)]
+
+# Publish
+DELETE_OUTPUT_DIRECTORY = False
+
+USER_LOGO_URL = 'https://ibb.co/gFF6tnw'
+
+# Sitemap
+SITEMAP_SAVE_AS = 'sitemap.xml'
+DIRECT_TEMPLATES = ['sitemap', 'index']
 
 DEFAULT_PAGINATION = False
 
